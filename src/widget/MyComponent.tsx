2c6f5df0df5f7cab6a9c175140c26d578387bded
Roman Ivanov <rivanov@megaputer.ru>
2023-09-13 14:01:46 +0300

    Used real data

--- src/widget/MyComponent.tsx
+++ src/widget/MyComponent.tsx
@@ -2,65 +2,70 @@ import * as React from 'react';
 import { ApiRequestor, Table } from 'pa-typings';
 import { Bar, type BarConfig } from '@ant-design/plots';
 
+type DataType = {
+  country: string;
+  population: number;
+  year: number;
+};
+
 interface Props {
   requestor: ApiRequestor;
 }
 
 export const MyComponent: React.FC<Props> = ({ requestor }) => {
   const wrapperGuid = React.useRef<{ wrapperGuid: string }>({ wrapperGuid: '' });
-  const [rowCount, setRowCount] = React.useState(0);
-  const [rowColumn, setColumnCount] = React.useState(0);
-  const [values, setValues] = React.useState<Table>({ rowIDs: [] });
+  const dataRef = React.useRef<DataType[]>([]);
+  const graphRef = React.useRef<any>(null);
+
+  const getData = (year: number) => {
+    return dataRef.current
+      .filter(row => row.year === year && row.country !== 'World')
+      .slice(0, 10);
+  };
 
   React.useEffect(() => {
     const fetchData = async () => {
       const guid = wrapperGuid.current = await requestor.wrapperGuid();
       let dsInfo = await requestor.info(guid);
 
-      setColumnCount(dsInfo.columns.length);
-      setRowCount(dsInfo.rowCount);
-
       const values = await requestor.values({
         offset: 0,
         rowCount: dsInfo.rowCount,
         wrapperGuid: guid.wrapperGuid
       });
-      setValues(values);
+
+      const data: DataType[] = [];
+      values.table?.forEach((row) => {
+        data.push({
+          country: String(row[0]),
+          population: Number(row[1]),
+          year: Number(row[2])
+        })
+      });
+
+      if (data.length > 0) {
+        dataRef.current = data;
+        graphRef.current.changeData(getData(data[0].year));
+      }
     };
     fetchData();
 
   }, [requestor]);
 
-  const data = [
-    {
-      year: '1951 年',
-      value: 38,
-    },
-    {
-      year: '1952 年',
-      value: 52,
-    },
-    {
-      year: '1956 年',
-      value: 61,
-    },
-    {
-      year: '1957 年',
-      value: 145,
-    },
-    {
-      year: '1958 年',
-      value: 48,
-    },
-  ];
-
-  const config = React.useMemo(() => ({
-    data,
-    xField: 'value',
-    yField: 'year',
-    seriesField: 'year',
+  const config: BarConfig = React.useMemo(() => ({
+    data: [],
+    xField: 'population',
+    yField: 'country',
+    seriesField: 'country',
     legend: false,
+    onReady: (bar) => {
+      graphRef.current = bar;
+    }
   }), []);
 
-  return <div><Bar {...config} /></div>;
+  return (
+    <div style={{ width: '100%', height: '100%' }}>
+      <Bar {...config} />
+    </div>
+  );
 }
